903ec0727e8a3ee02c44d0f8b2b126970b66180d
veedata <43007623+veedata@users.noreply.github.com>
2024-09-30 07:58:09 +0000

    Deploying to gh-pages from @ veedata/veedata.github.io@5ae93ebce91eb30d794e2efecd6d82392fc9f535 🚀

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){var r=$(".grid").masonry({gutter:10,horizontalOrder:!0,itemSelector:".grid-item"});r.imagesLoaded().progress(function(){r.masonry("layout")})});
+$(document).ready(function(){var o=$(".projects").masonry({horizontalOrder:!0,itemSelector:".col"});o.imagesLoaded().progress(function(){o.masonry("layout")})});
